483145a1dd1a6800a12305b2eceea9a5685fdf9e
cclark11 <courtneyarc@gmail.com>
2016-09-19 19:20:30 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+for (var i=1; i<=100;i++){
+ if(i%15==0){
+    $("body").append("fizzbuzz<br>")
+  }
+  else if(i%3==0){
+   $("body").append("fizz<br>")
+  }
+  else if(i%5==0){
+   $("body").append("buzz<br>")
+  }
+  else{
+    $("body").append(i+"<br>")
+  }
+}
